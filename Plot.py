3a746104079c7eccf09041c364574a34538a816d
KLesniak <mlodyrysiek@gmail.com>
2022-11-16 21:47:24 +0100

    add flask

--- Plot.py
+++ Plot.py
@@ -1,1 +1,19 @@
-print("This line will be printed.")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World!'
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    # run() method of Flask class runs the application
+    # on the local development server.
+    app.run(debug=True, host='0.0.0.0', port=5000)
